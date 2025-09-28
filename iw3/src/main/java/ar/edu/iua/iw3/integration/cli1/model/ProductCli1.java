1f6318161348058ea8db89a0f84a25d0c6e511db
Maximo Teyo <maxiiteyo@gmail.com>
2025-09-28 20:33:40 -0300

    clase 23/09

--- iw3/src/main/java/ar/edu/iua/iw3/integration/cli1/model/ProductCli1.java
+++ iw3/src/main/java/ar/edu/iua/iw3/integration/cli1/model/ProductCli1.java
@@ -1,6 +1,7 @@
 package ar.edu.iua.iw3.integration.cli1.model;
 
 import ar.edu.iua.iw3.model.Product;
+import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.PrimaryKeyJoinColumn;
@@ -12,6 +13,7 @@ import lombok.Setter;
 
 @Entity
 @Table(name= "cli1_products")
+@AttributeOverride(name="product", column=@Column(name="product_cli1", length = 150, unique = true, nullable = false))
 @PrimaryKeyJoinColumn(name = "id_product")
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,5 +22,5 @@ import lombok.Setter;
 public class ProductCli1 extends Product{
 
     @Column(nullable=false, unique=true)
-    private String codCli1;
+    private String codCli1; 
 }
